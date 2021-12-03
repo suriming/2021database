c97030d3274238889e6d7c87d0e7a6688eb83848
koy4648 <80434024+koy4648@users.noreply.github.com>
2021-12-03 19:36:40 +0900

    Add files via upload

--- front/pages/mylocation.vue
+++ front/pages/mylocation.vue
@@ -1,6 +1,63 @@
 <template>
   <v-container>
-    
+    <v-card
+    elevation="1"
+    outlined
+    shaped
+    >
+  <v-img :src="require('./1.jpg')"
+      height="300px"
+    ></v-img>
+  <v-card-title>
+      <nuxt-link to="./">공학관</nuxt-link>
+    </v-card-title>
+
+    </v-card>
+
+     <v-card
+    elevation="1"
+    outlined
+    shaped
+    >
+
+  <v-img :src="require('./2.jpg')"
+      height="300px"
+    ></v-img>
+      <v-card-title>
+      <nuxt-link to="./">백양관</nuxt-link>
+    </v-card-title>
+    </v-card>
+
+ <v-card
+    elevation="1"
+    outlined
+    shaped
+    >
+  
+  <v-img :src="require('./3.png')"
+      height="300px"
+    ></v-img>
+  <v-card-title>
+      <nuxt-link to="./">학생회관</nuxt-link>
+    </v-card-title>
+    </v-card>
+
+
+     <v-card
+    elevation="1"
+    outlined
+    shaped
+    >
+
+  <v-img :src="require('./4.jpg')"
+      height="300px"
+    ></v-img>
+       <v-card-title>
+      <nuxt-link to="./">신촌역</nuxt-link>
+    </v-card-title>
+    </v-card>
+
+
     <bottom-nav />
   </v-container>
 </template>
