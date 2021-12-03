190f075d34118f7432e339e118391cdff590d0dc
koy4648 <80434024+koy4648@users.noreply.github.com>
2021-12-03 18:08:12 +0900

    Add files via upload

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,47 +1,22 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll>
-      <v-spacer></v-spacer>
-
-          <v-toolbar-title color="#3F51B5">
-            <nuxt-link to ="/">연세톡</nuxt-link>
-          </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-main class="grey lighten-3">
-      <v-spacer></v-spacer>
-      <v-container fill-height justify-center>
-        <v-row align="center" justify-center>
-        <v-col cols="12">
-            <v-sheet
-            min-height="70vh"
-            rounded="lg"
-            >
-              <nuxt />
-            </v-sheet>
-        </v-col>
-        </v-row>
-      </v-container>   
-    </v-main> 
+  <v-app>
+    <Mynavbar>
+    </Mynavbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
-</template>
-
+</template>   
 
 <script>
-
+import Mynavbar from "./Mynavbar" 
   export default {
-      head() {
-          return {
-              title: '연세톡',
-          };
-      },
-    }
+    name: 'default',
+    components : {Mynavbar},
+    data(){
+      return {
 
+      }
+    } 
+  }
 </script>
-
-<style>
-
-</style>
