37bcf850f5d0c361e37ee1d411099749e1c8125a
koy4648 <80434024+koy4648@users.noreply.github.com>
2021-12-07 22:55:04 +0900

    Update friendpage.vue

--- front/pages/friendpage.vue
+++ front/pages/friendpage.vue
@@ -5,6 +5,7 @@
   >
     <v-list subheader>
       <v-subheader>내 정보</v-subheader>
+      <v-btn dark color="blue" nuxt to="/edit" type="submit">내 정보 수정</v-btn>
 
       <v-list-item
         v-for="chat in recent"
@@ -34,10 +35,12 @@
     <v-list subheader>
       <v-subheader>접속중인 친구</v-subheader>
 
-      <!-- <v-list-item> -->
-        <total-friend-list :users = "friendList" />
+      <v-list-item
+        v-for="chat in previous"
+        :key="chat.title"
+      >
         <!-- <v-list-item-avatar> -->
-          <!-- <total-friend-list /> -->
+          <friend-list />
           <!-- <v-img
             :alt="`${chat.title} avatar`"
             :src="chat.avatar"
@@ -47,7 +50,7 @@
         <v-list-item-content>
           <v-list-item-title v-text="chat.title"></v-list-item-title>
         </v-list-item-content> -->
-      <!-- </v-list-item> -->
+      </v-list-item>
     </v-list>
   <v-container>
     <bottom-nav />
@@ -57,25 +60,11 @@
 
 <script>
 import BottomNav from '../components/BottomNav.vue';
-import TotalFriendList from '../components/TotalFriendList.vue';
+import FriendList from '../components/TotalFriendList.vue';
   export default {
     components: {
         BottomNav,
-        TotalFriendList,
-    },
-  data() {
-    return{
-      valid: false, 
-      nickname: '',
+        FriendList,
     }
-  },
-  computed: {
-    friendList() {
-      return this.$store.state.users.friendList;
-    }
-  },
-  fetch({ store }) {
-    store.dispatch('users/loadFriend');
-  }
   }
 </script>
