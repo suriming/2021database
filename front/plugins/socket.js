c5ce06ae65fffd1e68200b0317c1112847da3436
koy4648 <80434024+koy4648@users.noreply.github.com>
2021-12-09 04:28:49 +0900

    Create socket.js

---
+++ front/plugins/socket.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+//import VueSocketIO from 'vue-socket.io'
+import io from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io-extended'
+
+export default ({ store }) => {
+  Vue.use(VueSocketIO, io('/'),
+    {
+      store,
+      debug: false,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+    })
+}
