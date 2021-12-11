8dcf8b65efe4bb94c60955febc6128b8dfdef29a
suriming <87427848+suriming@users.noreply.github.com>
2021-12-11 22:39:00 +0900

    Add files via upload

--- back/modules/db.js
+++ back/modules/db.js
@@ -1,27 +1,36 @@
 const mysql = require('mysql');
+
 const connection = mysql.createPool({
     host: '165.132.105.26',
     user: 'team06',
-    password: 'project06', // 이전에 입력했던 비밀번호
-    database: 'database06', // 이전에 입력했던 데이터베이스 명
+    password: 'project06', // DB password
+    port: 3306,
+    database: 'database06', // DB name
     multipleStatements: true,
     connectionLimit: 1000,
 });
 
-// query 함수는 db에 sql을 날리고 결과 값을 return 하는 함수입니다
+/*
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'test', // DB password
+    port: 3306,
+    database: 'db', // DB name
+    multipleStatements: true,
+    connectionLimit: 1000,
+});
+*/
+
+// Send SQL to DB and Receive return value
+// Promise --> Async
+// Await --> Block
 exports.query = query => new Promise((resolve, reject) => {
     connection.getConnection((err, connection) => {
-        if (err) {
-            console.log('실패1');
-            return reject(err);
-        }
-
+        if(err) return reject(err);
         return connection.query(query, (err2, rows) => {
             connection.release();
-            if (err2) {
-                console.log('실패2');
-                return reject(err2);
-            }
+            if(err2) return reject(err);
             return resolve(rows);
         })
     });
